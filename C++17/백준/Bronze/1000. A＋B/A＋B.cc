55457553e62227fc096dffa2bec11259fb853994
Chun-Bae <69321105+Chun-Bae@users.noreply.github.com>
2024-06-20 02:24:46 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+void init() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+}
+
+int main() {
+    init();
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+    return 0;
+}
