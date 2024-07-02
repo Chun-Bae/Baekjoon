fe5e040b49032ad73468f310871368ce4f249b98
Chun-Bae <69321105+Chun-Bae@users.noreply.github.com>
2024-07-02 15:13:21 +0900

    [Gold II] Title: 트리의 지름, Time: 84 ms, Memory: 10708 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,63 @@
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<pair<int, int>> tree[100001];
+bool visited[100001];
+int maxDistance;
+int maxNode;
+
+void init() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+}
+
+void dfs(int node, int distance) {
+    visited[node] = true;
+
+    if (distance > maxDistance) {
+        maxDistance = distance;
+        maxNode = node;
+    }
+
+    for (auto &edge : tree[node]) {
+        int nextNode = edge.first;
+        int weight = edge.second;
+        if (!visited[nextNode]) {
+            dfs(nextNode, distance + weight);
+        }
+    }
+}
+
+int main() {
+    init();
+    cin >> n;
+
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v, w;
+        cin >> u;
+        while (1) {
+            cin >> v;
+            if (v == -1) break;
+            cin >> w;
+            tree[u].emplace_back(v, w);
+            tree[v].emplace_back(u, w);
+        }
+    }
+
+    memset(visited, false, sizeof(visited));
+    maxDistance = 0;
+    dfs(1, 0);
+
+    memset(visited, false, sizeof(visited));
+    maxDistance = 0;
+    dfs(maxNode, 0);
+
+    cout << maxDistance << '\n';
+
+    return 0;
+}
