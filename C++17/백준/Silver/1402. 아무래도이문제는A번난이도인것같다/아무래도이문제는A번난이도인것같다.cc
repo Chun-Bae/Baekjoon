b8077af2aad4beea174d3231ee1af3b83f556dcd
Chun-Bae <69321105+Chun-Bae@users.noreply.github.com>
2024-08-03 10:17:19 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T, A, B;
+    cin >> T;
+    while (T--) {
+        cin >> A >> B;
+        cout << "yes" << endl;
+    }
+    return 0;
+}
