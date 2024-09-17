f9d59949c29714686813dd5882487fe22b4568d1
Chun-Bae <69321105+Chun-Bae@users.noreply.github.com>
2024-09-18 01:25:34 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 128 ms, Memory: 41092 KB -BaekjoonHub

---
+++ C++17/백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+const ll MOD = 1000000007;
+ll N, M, K;
+vector<ll> arr;
+vector<ll> tree;
+
+void init() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+}
+
+ll segtree(ll node, ll start, ll end) {
+    if (start == end) {
+        return tree[node] = arr[start] % MOD;
+    } else {
+        ll mid = (start + end) / 2;
+        return tree[node] = (segtree(node * 2, start, mid) * segtree(node * 2 + 1, mid + 1, end)) % MOD;
+    }
+}
+
+void update(ll node, ll start, ll end, ll idx, ll val) {
+    if (idx < start || idx > end) return;
+    if (start == end) {
+        arr[idx] = val % MOD;
+        tree[node] = val % MOD;
+        return;
+    }
+    ll mid = (start + end) / 2;
+    update(node * 2, start, mid, idx, val);
+    update(node * 2 + 1, mid + 1, end, idx, val);
+    tree[node] = (tree[node * 2] * tree[node * 2 + 1]) % MOD;
+}
+
+ll query(ll node, ll start, ll end, ll l, ll r) {
+    if (r < start || end < l) return 1;
+    if (l <= start && end <= r) return tree[node] % MOD;
+    ll mid = (start + end) / 2;
+    ll p1 = query(node * 2, start, mid, l, r);
+    ll p2 = query(node * 2 + 1, mid + 1, end, l, r);
+    return (p1 * p2) % MOD;
+}
+
+int main() {
+    init();
+    cin >> N >> M >> K;
+    arr.resize(N);
+    tree.resize(N * 4);
+    for (ll i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    segtree(1, 0, N - 1);
+
+    for (ll i = 0; i < M + K; i++) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) {
+            update(1, 0, N - 1, b - 1, c);
+        } else if (a == 2) {
+            ll result = query(1, 0, N - 1, b - 1, c - 1);
+            cout << result << '\n';
+        }
+    }
+
+    return 0;
+}
