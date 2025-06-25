3d1d25468b826f157bb3847fd2bba5e3a8914b0c
Chun-Bae <69321105+Chun-Bae@users.noreply.github.com>
2025-06-25 09:32:16 +0900

    [Silver II] Title: 주식, Time: 2712 ms, Memory: 153700 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,16 +1,15 @@
 T=int(input())
 
 for _ in range(T):
-    N=int(input())
-    prices=list(map(int, input().split()))
+    N = int(input())
+    prices= list(map(int,input().split()))
 
     max_price=0
     profit=0
-
     for price in reversed(prices):
-        if price>max_price:
+        if max_price < price:
             max_price=price
-        else:
-            profit += (max_price-price)
+        
+        profit += (max_price-price)
     
     print(profit)
