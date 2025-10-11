3d2bfeb440b55f3a35061d1f0d6158f71b043985
Chun-Bae <69321105+Chun-Bae@users.noreply.github.com>
2025-10-11 23:04:41 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -5,27 +5,23 @@
 using namespace std;
 
 int main() {
-    vector<int> stair;
-    vector<int> max_stairs;
-    int T;
-    cin >> T;
-    max_stairs.resize(T + 1, 0);
-    stair.resize(T + 1, 0);
-    for (int i = 1; i <= T; i++)
-        cin >> stair[i];
+    int N;
+    cin >> N;
+    vector<int> stairs(N + 1);
+    vector<int> dp(N + 1, 0);
+    for (int i = 1; i <= N; i++)
+        cin >> stairs[i];
 
-    max_stairs[1] = stair[1];
-    max_stairs[2] = stair[1] + stair[2];
-    max_stairs[3] = max(stair[1] + stair[3], stair[2] + stair[3]);
+    dp[1] = stairs[1];
+    dp[2] = stairs[1] + stairs[2];
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
 
-    for (int i = 4; i <= T; i++) {
-        /// x1: 현재 밟은 게 2칸 뛰어서 한 번에 온건지,
-        /// x2: 현재 밟은 게 이전에 밟고 온건지, -> 조건1에 위배되는 경우를
-        ///     남기지 않기 위해 그 이전에 2칸(i-3) 밟고 i-1을 밟은 걸로 확신해줌.
-        int x1 = max_stairs[i - 2] + stair[i];
-        int x2 = max_stairs[i - 3] + stair[i - 1] + stair[i];
-        max_stairs[i] = max(x1, x2);
+    for (int i = 4; i <= N; i++) {
+        int a = stairs[i] + dp[i - 2];
+        int b = stairs[i] + stairs[i - 1] + dp[i - 3];
+        dp[i] = max(a, b);
     }
-    cout<<max_stairs[T];
+    cout << dp[N];
+
     return 0;
 }
