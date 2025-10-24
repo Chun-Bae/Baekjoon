a88af20053ce625bd4d446dc5dbcc3a819279bb2
Chun-Bae <69321105+Chun-Bae@users.noreply.github.com>
2025-10-24 17:38:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,44 +1,30 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-void bfs(vector<vector<int>> adj, int* visited, int V) {
-    queue<int> q;
-    q.push(V);
-    visited[V] = 1;
-    while (!q.empty()) {
-        int target = q.front();
-        printf("%d ", target);
-        q.pop();
+int N, M, V;
+vector<vector<int>> adj;
+vector<int> visited;
 
-        for (auto s : adj[target]) {
-            if (visited[s] == 0) {
-                q.push(s);
-                visited[s] = 1;
-            }
+void dfs(int cur) {
+    cout << cur << " ";
+    for (int next : adj[cur]) {
+        if (!visited[next]) {
+            visited[next] = 1;
+            dfs(next);
         }
     }
-}
-
-void dfs(vector<vector<int>> adj, int* visited, int target) {
-    if(visited[target]==1) return;
-    visited[target]=1;
-    printf("%d ",target);
-    for(auto s : adj[target]){
-        dfs(adj,visited,s);
-    }
+    return;
 }
 
 int main() {
-    int N, M, V;
-    int visited_b[1001] = {};
-    int visited_d[1001] = {};
-    vector<vector<int>> adj(1001);
-
     cin >> N >> M >> V;
+    adj.assign(N + 1, {});
+    visited.assign(N + 1, 0);
+
     for (int i = 0; i < M; i++) {
         int u, v;
         cin >> u >> v;
@@ -48,10 +34,29 @@ int main() {
     for (int i = 1; i <= N; i++) {
         sort(adj[i].begin(), adj[i].end());
     }
+    /// DFS
+    visited.assign(N + 1, 0);
+    visited[V] = 1;
+    dfs(V);
+    cout << endl;
+    /// BFS
+    visited.assign(N + 1, 0);
+    queue<int> q;
 
-    dfs(adj, visited_d, V);
-    cout<<endl;
-    bfs(adj, visited_b, V);
+    q.push(V);
+    visited[V] = 1;
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        cout << cur << " ";
+
+        for (int next : adj[cur]) {
+            if (!visited[next]) {
+                visited[next] = 1;
+                q.push(next);
+            }
+        }
+    }
 
     return 0;
 }
